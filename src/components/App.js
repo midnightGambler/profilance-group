8cbc80aa12fe2a57dc6ab0af6abcbe233f23a726
midnightGambler <42617615+midnightGambler@users.noreply.github.com>
2020-10-01 02:29:11 +0300

    started setting up store

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "../store/store";
 import { Header } from "./Header/Header";
 import { MainPage } from "../pages/MainPage/MainPage";
 import { NewsPage } from "../pages/MainPage/NewsPage";
 
 export const App = () => (
-  <Router>
-    <Header />
-    <Switch>
-      <Route path="/" exact component={MainPage} />
-      <Route path="/news" exact component={NewsPage} />
-    </Switch>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={MainPage} />
+        <Route path="/news" exact component={NewsPage} />
+      </Switch>
+    </Router>
+  </Provider>
 );
---
+++ src/reducers/auth.js
@@ -0,0 +1,10 @@
+const defaultState = {
+  isAuthorized: false,
+};
+
+export const authReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
